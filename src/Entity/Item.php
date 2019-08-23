23f6c6393a9ea7bb1a9a720b0cb2e5b21015cb01
Kévin Dunglas <dunglas@gmail.com>
2019-08-23 17:34:38 +0200

    First functional test

---
+++ src/Entity/Item.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=2048)
+     */
+    private $storyLink;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $publishedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStoryLink(): ?string
+    {
+        return $this->storyLink;
+    }
+
+    public function setStoryLink(string $storyLink): self
+    {
+        $this->storyLink = $storyLink;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
---
+++ tests/ItemControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ItemControllerTest extends WebTestCase
+{
+    public function testCreateItem(): void
+    {
+        $client = static::createPantherClient();
+        // Load the index
+        $client->request('GET', '/item/');
+        $client->followRedirects();
+
+        $this->assertPageTitleSame('Item index');
+        $client->clickLink('Create new');
+
+        // On the creation form
+        $this->assertPageTitleSame('New Item');
+        $this->assertSelectorTextSame('body > h1', 'Create new Item');
+
+        // Fill and submit the form
+        $client->submitForm('Save', [
+            // item[storyLink] is the name of the form field
+            'item[storyLink]' => 'https://masterclass.les-tilleuls.coop',
+            'item[publishedAt][date][month]' => '9',
+            'item[publishedAt][date][day]' => '18',
+            'item[publishedAt][date][year]' => '2019',
+        ]);
+
+        $this->assertSelectorTextContains('table', 'https://masterclass.les-tilleuls.coop');
+    }
+}
